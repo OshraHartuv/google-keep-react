0d7c37c9bd8a2127635cdb4ef902c521bad61fd7
Oshra Hartuv <oshrartuv@gmail.com>
2023-02-20 10:08:23 +0200

    changed icons and light/dark search bar

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,51 +1,39 @@
-import { RxGear, RxReload } from 'react-icons/rx';
-import { TbGridDots } from 'react-icons/tb';
-import { TfiViewList } from 'react-icons/tfi';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { IoIosMenu } from 'react-icons/io';
 import { GrClose } from 'react-icons/gr';
+import {
+    appsIcon,
+    menuIcon,
+    refreshIcon,
+    searchIcon,
+    settingsIcon,
+} from '../assets/svg/svgIcons';
 export function AppHeader() {
     return (
         <header className="app-header flex full">
-            <div className="tools">
-                <img
-                    className="avatar"
-                    src="https://lh3.googleusercontent.com/ogw/AAEL6shCk1ZY60rWoW7uIVQrdJVCYZoHIPLO4DbwMUAG=s32-c-mo"
-                />
-
-                <button className="apps icon">
-                    <TbGridDots />
-                </button>
-                <button className="gear icon">
-                    <RxGear />
-                </button>
-                <button className="list icon">
-                    <TfiViewList />
-                </button>
-                <button className="reload icon">
-                    <RxReload />
-                </button>
-            </div>
-            <div className="search">
-                <span className="icon">
-                    <GrClose />
-                </span>
-                <input type="text" />
-                <span className="icon">
-                    <AiOutlineSearch />
-                </span>
-            </div>
             <div className="menu">
+                <button className="icon">{menuIcon('24px', '24px')}</button>
                 <div className="logo">
-                    <span>Keep</span>
                     <img
                         className="keep-logo"
                         src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
                     />
+                    <span>Keep</span>
                 </div>
-                <button className="icon">
-                    <IoIosMenu />
-                </button>
+            </div>
+            <div className="search">
+                <span className="icon search-icon">{searchIcon()}</span>
+                <input type="text" placeholder="search" />
+                <span className="icon close">
+                    <GrClose />
+                </span>
+            </div>
+            <div className="tools">
+                <button className="icon">{refreshIcon()}</button>
+                <button className="icon">{settingsIcon()}</button>
+                <button className="icon">{appsIcon()}</button>
+                <div className="avatar">
+                    <img src={require('../assets/imgs/doggo.jpg')} />
+                </div>
             </div>
         </header>
     );
